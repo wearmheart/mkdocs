9d1e4ecab532d02eeee559b993fe88634377ad17
fengwen <3534800157@qq.com>
2023-02-24 00:41:58 +0800

    2023-02-34

---
+++ days.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/warmheart/mkdocs
+
+# 获取当前时间戳并将其转换为可读的时间格式
+now=$(date -d @$(date +%s) +"%Y-%m-%d %H:%M:%S")
+
+echo "当前时间是：$now"
+
+git add .
+
+git commit -m "${now}"
+
+git push 
