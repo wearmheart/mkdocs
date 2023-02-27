ae3aa89abdd43fd9bbdc104c5ba2b62f73d187eb
fengwen <3534800157@qq.com>
2023-02-27 08:56:45 +0800

    2023-02-27

--- days.sh
+++ days.sh
@@ -3,14 +3,31 @@ cd /home/warmheart/mkdocs
 git pull 
 
 # 获取当前时间戳并将其转换为可读的时间格式
-now=$(date -d @$(date +%s) +"%Y-%m-%d %H:%M:%S")
+now=$(date -d @$(date +%s) +"%Y-%m-%d")
 
-echo "当前时间是：$now"
+git checkout -b $now 
+
+sel=$1
+
+echo "当前时间是：$now" 
+
+
+if test "${sel}" -eq "1";
+then
+    echo "touch ${now}_TODE.md"
+    touch ${now}_TODE.md 
+fi 
 
 git add .
 
 git commit -m "${now}"
- 
-git   push  
+
+if test "$sel" = "1";then
+    git push --set-upstream origin $now  
+else
+    git push  
+fi
+
+
 
 # mkdocs gh-deploy
